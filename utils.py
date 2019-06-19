a26358a070da63a739fdb5c1d9ba1369b5df2284
iamkroot <kroot.patel@gmail.com>
2019-06-19 15:28:08 +0530

    Initial commit

---
+++ utils.py
@@ -0,0 +1,23 @@
+import json
+
+with open('config.json') as f:
+    cfg = json.load(f)
+
+
+def clean(d):
+    if isinstance(d, list):
+        for a in d:
+            clean(a)
+    elif isinstance(d, dict):
+        for k, v in d.copy().items():
+            if not v or v == ' - ' or v == '-':
+                del d[k]
+    return d
+
+
+def parse_resp(resp):
+    return clean(json.loads(resp.text[6:-2]
+                            .replace('\\"', '"')
+                            .replace('\\n', '\n')
+                            .encode()
+                            .decode('unicode-escape')))
